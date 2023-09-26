3ad7f69f9f232d78194aaf32ec7816c56cc87c72
Ruthvik-Madavaram <82954285+Ruthvik-Madavaram@users.noreply.github.com>
2023-09-26 11:36:50 +0530

    feat: builder design pattern (#2)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import builder.Payslip;
 import factory.Order;
 import factory.OrderFactory;
+
 import singleton.Singleton;
 
 public class Main {
     public static void main(String[] args) {
         Singleton singleton = new Singleton();
         singleton.demo();
+
+        //Builder Pattern
+        Payslip payslip = new Payslip.PayslipBuilder()
+                .setEmployeeName("Ruthvik")
+                .setEmployeeId("123")
+                .setBasic(50000)
+                .setDa(5000)
+                .setHra(5000)
+                .setTax(10000)
+                .setNetSalary(55000)
+                .build();
+        System.out.println(payslip);
 
         try {
             //Factory Design Pattern
